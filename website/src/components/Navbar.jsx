64eabf284b280b629d76218426c7e1044e8355ff
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-10-11 22:40:10 +0530

    Created Hero section

---
+++ website/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className='fixed top-0 left-0 z-50 md:p-9 p-3'>
+        <img className='md:w-24 w-20' src="/images/nav-logo.svg" alt="nav-logo" />
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar;
