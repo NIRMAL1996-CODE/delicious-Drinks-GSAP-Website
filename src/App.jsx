3490f532e551123e748995a12cf9d308f92420ed
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-10-15 22:29:02 +0530

    ScrollSmoother added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+
 import Navbar from './components/Navbar';
 import HeroSection from './sections/HeroSection';
 import gsap from 'gsap';
 import { ScrollTrigger,ScrollSmoother } from "gsap/all";
-// import { useGSAP } from '@gsap/react';
+import { useGSAP } from '@gsap/react';
 import Messagesection from './sections/Messagesection';
 import Flavoursection from './sections/Flavoursection';
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 const App = () => {
+  useGSAP(()=>{
+   ScrollSmoother.create({
+    smooth:3,
+    effects:true,
+   });
+  });
   return (
-    <div>
+    <main>
       <Navbar/>
-      <HeroSection/>
-      <Messagesection/>
-      <Flavoursection/>
-    </div>
+      <div id='smooth-wrapper'>
+        <div id='smooth-content'>
+          <HeroSection/>
+          <Messagesection/>
+          <Flavoursection/>
+        </div>
+      </div>
+    </main>
   )
 }
 
